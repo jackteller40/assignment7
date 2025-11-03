746f0646771b911ad9dedb37f51a9443099279a1
Jack Teller <jack.teller1@marist.edu>
2025-11-02 19:55:23 -0500

    Create Enemy.Java

---
+++ Enemy.Java
@@ -0,0 +1,13 @@
+public class Enemy {
+    int health;
+    int damage;
+
+    public Enemy(int h, int d){
+        health = h;
+        damage = d;
+    }
+
+    void attack(){
+        System.out.println("the enemy attacks");
+    }
+}
